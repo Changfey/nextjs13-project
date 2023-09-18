56690d5f4c8e68ed8d99d991b6e6e35ab18dc7ea
张鲱 <rocinante1453@gmail.com>
2023-09-18 19:11:24 +1000

    add the type of todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { TodoItem } from "@/components/TodoItem";
 import { prisma } from "@/db";
+import { Todo } from "@/typing";
 import Link from "next/link";
 
 function getTodos() {
@@ -12,25 +13,24 @@ export default async function Home() {
   async function toggleTodo(id: string, complete: boolean) {
     "use server"
 
-    await prisma.todo.update({ where: { id }, data: { complete }})
-
+    await prisma.todo.update({ where: { id }, data: { complete } })
   }
 
   return <>
-  <header className="flex justify-between items-center mb-4">
-    <h1 className="text-2xl">Todos</h1>
-    <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-width:bg-slate-700 outline-none"
-    href="/new">New</Link>
-  </header>
-  <ul className="p1-4">
-    {todos.map(todo => (
-      <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
-    ))
+    <header className="flex justify-between items-center mb-4">
+      <h1 className="text-2xl">Todos</h1>
+      <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-width:bg-slate-700 outline-none"
+        href="/new">New</Link>
+    </header>
+    <ul className="p1-4">
+      {
+        (todos as Todo[]).map((todo: Todo) => (
+          <TodoItem key={todo.id}  {...todo} toggleTodo={toggleTodo} />
+        ))
+      }
 
-    }
+    </ul>
 
-  </ul>
-  
-  
+
   </>
 }
---
+++ src/typing.ts
@@ -0,0 +1,7 @@
+export type Todo = {
+    id: string;
+    title: string;
+    complete: boolean;
+    createdAt: any;
+    updatedAt: any;
+}
